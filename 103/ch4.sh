6b500e7fe84fb4572d3283ac1b37cfbbecd00791
Josh F <jay.f911@gmail.com>
2023-11-09 19:56:30 -0500

    ch4

---
+++ 103/ch4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Lets create a function in bash that adds two number together
+# Stretch goal can you do subtraction, multipliaction or division
+# You will need to declare two variables
+num1=2
+num2=3
+echo $[num1 + num2]
+5
