78dcc23183b9d4c0e7d2db51cb307688652c9b3e
Josh F <jay.f911@gmail.com>
2023-11-28 19:36:07 -0500

    ch12

---
+++ 103/ch12.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Today we are going to use a case statment instead of a conditional
+# Have the program ask how your day is and echo out a response for good or bad
+# Case statment format is a little different so please look up how this would be formated
+
+echo "hows your day? Please select option 1 for good and option 2 for bad"
+read response
+
+case "$response" in
+    "good")
+    echo (great)
+    ;;
+    "bad")
+    echo "terrible"
+    ;;
+    *)
+    echo "sorry, I didnt understand"
+    ;;
+esac
