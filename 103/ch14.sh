ccfb706f31887dc9ed0dbf482f72b38f84a9aafc
Josh F <jay.f911@gmail.com>
2023-12-06 19:11:56 -0500

    ch14

---
+++ 103/ch14.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Create a script that you think you would use in a daily job routine No right or Wrong anwsers here
+# You could ping all the devices in your network?
+# Run a script to reset your ip address?
+# Create a script that does some math? 
+
+echo "Top of the morning to ya"
+sleep 2
+echo $(curl wttr.in/)
+
+
